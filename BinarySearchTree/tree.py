6075052a09dacfd94e162b352e2d08e11ab9bb21
unknown <kristiandamsgaard@gmail.com>
2019-04-27 09:39:48 +0200

    Added binary search tree

---
+++ BinarySearchTree/tree.py
@@ -0,0 +1,16 @@
+#
+# Kristian Knudsen Damsgaard, 2019
+#
+# A standard binary search tree.
+#
+# 
+
+class BST:
+
+    def __init__(self):
+        self._root = Node()
+
+
+class Node:
+    def __init__(self):
+        _id = 1
