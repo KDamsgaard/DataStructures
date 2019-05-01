3ab226bcfbd294988c3553481a1104422760902e
unknown <kristiandamsgaard@gmail.com>
2019-05-01 18:21:12 +0200

    Messed everything up

--- BinarySearchTree/binary_search_tree.py
+++ BinarySearchTree/binary_search_tree.py
@@ -1,7 +1,7 @@
 """
 Kristian Knudsen Damsgaard, 2019
 
-The basic binary search tree components for the balanced binary search tree implementation.
+The basic binary search tree components for the balanced binary search tree implementations (AVL and RedBlack).
 """
 
 from node import Node
@@ -12,13 +12,15 @@ class BinarySearchTree():
         self.__root = None
         self.__errors = []
 
-    def error_handler(self, node):
-        error = str(datetime.now()) + ": " + str(node.get_data())
-        self.__errors.append(error)
-        print(error)
-
     def insert_node(self, key, data) -> Node:
+        """
+        Inserts a new node into the tree. Checks for non-integer and duplicate keys.
 
+        @type key: integer
+        @param key: The key of the node to insert
+        @type data: any
+        @param data: The data to include in the inserted node
+        """
         try:
             key == int(key)
             new_node = Node(key, data)
@@ -32,29 +34,80 @@ class BinarySearchTree():
         else:
             current_node = self.__root
             while True:
-                if current_node.get_key() == key:
-                    error = Node("error", "insert_node() error: Duplicate key.")
-                    self.error_handler(error)
-                    return error
-                elif current_node.get_key() < key:
+                if current_node.get_key() < key:
                     if current_node.get_right() is None:
                         current_node.set_right(new_node)
                         new_node.set_parent(current_node)
+                        new_node.calc_and_set_height(new_node)
                         return new_node
                     else:
                         current_node = current_node.get_right()
-                else:
+                elif current_node.get_key() > key:
                     if current_node.get_left() is None:
                         current_node.set_left(new_node)
                         new_node.set_parent(current_node)
+                        new_node.calc_and_set_height(new_node)
                         return new_node
                     else:
                         current_node = current_node.get_right()
+                else: #current_node.get_key() == key
+                    error = Node("error", "insert_node() error: Duplicate key.")
+                    self.error_handler(error)
+                    return error
+
+    # Rotation diagram
+    #     x     left -->    y
+    #     /\    <-- right  /\
+    #    A  y             x  C
+    #       /\           /\
+    #      B  C         A  B
+    
+    def left_rotate(self, key):
+
+        x = self.get_node(key)
+        y = x.get_right()
+
+        attachment_point = x.get_parent()
+
+        
+ 
+
+        #
+        x.set_right(y.get_left())
+        x.set_parent(y)
+
+
+        #right child becomes parent
+
+        #right child's left child becomes right child
+
+
+
+    
+    def right_rotate(self, key):
+        pass #TODO
+
+    def error_handler(self, node):
+        """
+        Straps the data from an error node returned by another method to the errors list.
+        The error node is a standard node set with "error" as key and error message as data.
+
+        @type node: Node
+        @param node: The error from which to get the error message
+        """
+        error = str(datetime.now()) + ": " + str(node.get_data())
+        self.__errors.append(error)
+        print(error)
 
     #GETTERS    -----------------------------------------------------------------------------------------------
 
     def get_node(self, key) -> Node:
+        """
+        Locates and returns the node belonging to supplied key. Checks for empty tree and non-existant key.
 
+        @type key: integer
+        @param key: The key of the node to return
+        """
         if self.__root == None:
             error = Node("Error", "get_node() error: Empty tree.")
             self.error_handler(error)
@@ -73,23 +126,27 @@ class BinarySearchTree():
                 error = Node("error", "Tried to get non-existant node.")
                 self.error_handler(error)
                 return error
-        
+    
+    def get_errors(self):
+        return self.__errors
     #PRINTERS   -----------------------------------------------------------------------------------------------
 
-    def print_node(self, key) -> Node:
+    def print_node(self, node) -> Node:
+        """
+        Prints information about a node.
 
-        if self.get_node(key).get_key() == "error":
-            error = Node("error", "print_node() error: Attempted to print non-Node object")
-            self.error_handler(error)
-            return error
+        @type node: Node
+        @param node: The node to print
+        """
 
-        node = self.get_node(key)
+        #TODO: Move to Node class
 
         key = str(node.get_key())
         data = str(node.get_data())
         parent_key = "None"
         left_key = "None"
         right_key = "None"
+        height = str(node.get_height())
 
         if node.get_parent() is not None:
             parent_key = str(node.get_parent().get_key())
@@ -97,16 +154,16 @@ class BinarySearchTree():
             left_key = str(node.get_left().get_key())
         if node.get_right() is not None:
             right_key = str(node.get_right().get_key())
+        
 
         result = "_____________________________________\n"
-        result += "" + key + ": " + data + "\n"
+        result += "" + key + ": " + data + ", height: " + str(height) + "\n"
         if node == self.__root:
             result += "ROOT "
         else:
             result += "Parent: " + parent_key + " "
         result += "Left: " + left_key + " Right: " + right_key
         print(result)
-
 
 #END OF CLASS "Node"    ---------------------------------------------------------------------------------------
 
@@ -121,11 +178,15 @@ tree.insert_node(1, "1_data")
 tree.insert_node(0, "0_data")
 tree.insert_node(2, "2_data")
 tree.insert_node(3, "3_data")
+tree.insert_node(4, "4_data")
 
-tree.print_node(0)
-tree.print_node(1)
-tree.print_node(2)
-tree.print_node(3)
+tree.print_node(tree.get_node(0))
+tree.print_node(tree.get_node(1))
+tree.print_node(tree.get_node(2))
+tree.print_node(tree.get_node(3))
+tree.print_node(tree.get_node(4))
+
+# print(tree.get_errors())
 
 
 
--- BinarySearchTree/node.py
+++ BinarySearchTree/node.py
@@ -11,6 +11,7 @@ class Node:
         self.__parent = None
         self.__left = None
         self.__right = None
+        self.__height = 0
 
     def __del__(self):
         self.__key = None
@@ -18,18 +19,33 @@ class Node:
         self.__parent = None
         self.__left = None
         self.__right = None
+        self.__height = None
+
+    def calc_and_set_height(self, node):
+        height = self.calculate_height(node)
+        self.set_height(height)
     
+    def calculate_height(self, node):
+        if node is None:
+            return 0
+        else:
+            print ("calcing")
+            return max(self.calculate_height(self.__left), self.calculate_height(self.__right)) + 1
+
+
 
     #SETTERS -------------------------------------------------------------
     def set_parent(self, node):
         self.__parent = node
+
+    def set_left(self, node):
+        self.__left = node
     
     def set_right(self, node):
         self.__right = node
 
-    def set_left(self, node):
-        self.__left = node
-
+    def set_height(self, height):
+        self.__height = height
     #GETTERS -------------------------------------------------------------
 
     def get_key(self):
@@ -46,5 +62,8 @@ class Node:
 
     def get_right(self):
         return self.__right
+    
+    def get_height(self):
+        return self.__height
         
 #END OF CLASS "Node" -----------------------------------------------------
